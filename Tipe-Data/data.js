99f9f412149b647d2c5526e37235d1acdd5da1ff
FairuzzIslami <firdauz2007123@gmail.com>
2024-01-21 22:01:14 +0700

    day 1

---
+++ Tipe-Data/data.js
@@ -0,0 +1,15 @@
+// Tipe data String "" ada petiknya
+alert("Hello world")
+document.write("fairuz aqila islmi" +" adzana shalihah alifiyah")
+
+
+// tipe data Number berbentuk angka dan gaka ada string nya /"" 
+// jika angka nya desimal 2,3 ==> 2.3
+document.write(100)
+document.write(2.3)
+
+// tipe data bolean
+// ada 2 data yaitu true dan false saja
+prompt(true)
+alert(false)
+
