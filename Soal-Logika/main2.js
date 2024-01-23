dd7da240c0a459b571baf72403609182fd05abc4
FairuzzIslami <firdauz2007123@gmail.com>
2024-01-23 23:01:47 +0700

    soal 2

---
+++ Soal-Logika/main2.js
@@ -0,0 +1,11 @@
+// membuat soal logika dari if else, dgn password yg benar "fairuz"
+// dan akan salah jika gak memasukan password fairuz
+
+var password = prompt("massukan password: ")
+
+if(password == "fairuz"){
+    console.log("benar")
+}
+else{
+    console.log("salah")
+}
