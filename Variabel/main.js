76350b7d1d7e759453dc5c0c20cd59003cac668c
FairuzzIslami <firdauz2007123@gmail.com>
2024-01-24 22:36:36 +0700

    Belajar varibel var,let,const

---
+++ Variabel/main.js
@@ -0,0 +1,38 @@
+// variabel adalah tempat untuk menyimpan data 
+
+
+
+var dcm = document.writeln;
+var nama = "fairuz aqila islami";
+nama = "ashel"
+
+// ketika terdapat dua variabel yg sama tetapi value isinya beda maka dia akan menginput 
+// yg terakhir ia tulis variabel nya
+
+document.writeln("hello " + nama); 
+
+
+// Wajib di ingat ketika kita ingin membuat sebuah variabel itu harus dari
+// atas, karena ketika ingin meng input misal alert,console.log,prompt 
+// itu bisa di akses
+// // contoh:
+// console.log("hello world") XX
+// nama = "damar artha"  XXX
+
+var n = "Fairuz"
+console.log(n)
+
+
+
+// ketika kita ingin membuat variabel jgn menggunakan var tetapi gunakan let dan const saja
+// karena .......
+let umur = 12
+let kelas = 10
+console.log("umur saya sekarang adalah" + umur )
+
+// keika menggunakan const itu variabel / data nya jangan di ubah ubah
+// karena jika di ubah akan eror
+
+const sekolah = "barkab"
+sekolah = "maha"
+console.log(sekolah)
