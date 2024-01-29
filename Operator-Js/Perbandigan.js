84d732c74089cf5c90c0a90a92bbfc86ea56fdec
FairuzzIslami <firdauz2007123@gmail.com>
2024-01-29 13:09:55 +0700

    nambah file baru

---
+++ Operator-Js/Perbandigan.js
@@ -0,0 +1,21 @@
+//operator perbandingan berfungsi untuk membandikan dua/2 nilai yang ingin
+//di bandingkan di sebuah kasus pemrograman
+// <  = lebih dari              <= = lebih dari sama degan
+// >  = kurang dari             >= = kurang dari sama dgn
+// == = sama dengan             === = sama dgn dan harus sesuai tipe nilai nya
+// != tdk sama dgn              !== = tdk sama dgn dan tidak setipe dgn nilai awalnya
+
+let perbandingan = 10 == "10"
+console.log(perbandingan)
+
+let angka = 10 === "10"  // karena dia tipe datanyta tidak sama harusnya number itu malah string jadi === 
+console.log(angka)       // wajub tipe datanya itu sama
+
+
+angka = 10 < 11
+console.log(angka)
+
+
+angka = 9 > 10
+console.log(angka)
+
