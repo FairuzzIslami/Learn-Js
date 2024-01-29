3cf62e74aa46739adcf92c8adab44266fe663cb5
FairuzzIslami <firdauz2007123@gmail.com>
2024-01-29 21:10:24 +0700

    nambah function dan for

---
+++ Soal-Logika/Perulangan.js
@@ -0,0 +1,13 @@
+const angka1 = [22,43,53,30,98,56,78,83,24,67,82,40,20]
+
+
+
+
+for(let i=12; i >0 ; i --){
+    // if (angka1[i] %2 == 0){
+    //     console.log("aku genap")
+    // }else{
+    //     console.log("salah")
+    // }
+    console.log("halllo aku adalah " + i + " nilai ku adalah " +angka1[i])                                                                                                                                                                                                                                         
+}
---
+++ Soal-Logika/soalfunction.js
@@ -0,0 +1,25 @@
+function angka(nilai){
+    // let nilai = prompt("massukkan nilai anda: ");
+    if( nilai <55 ){
+        return "blok"
+    }
+    else if( nilai >=55 && nilai <= 70){
+        return "ya ok aja"
+    }
+    else if (nilai >=71 && nilai <= 80 ){
+        return "ok cukup"
+    }
+    else if (nilai>= 81 && nilai <= 85){
+        return "busetttt"
+    }
+    else{
+        return "kelaszxxzz"
+    }
+}
+let nilai = 80
+let hasilNilai = angka(nilai)
+console.log(hasilNilai)
+
+//jika saya meng run menggunakan return nanti hasil nya gak keliatan karena
+//di dalam function nya sudah di isi oleh parameter return jadi kita harus 
+//menampilkan menggunakan parameter lainya 
