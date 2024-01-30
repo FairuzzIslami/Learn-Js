0f513a3329376cb188ef095d533241d205a430a6
FairuzzIslami <firdauz2007123@gmail.com>
2024-01-30 11:39:11 +0700

    selasa 30 januari 2024

---
+++ IF-ELSE/Baru.js
@@ -0,0 +1,10 @@
+let angka1 = 20;
+let angka2 = 20;
+
+if(angka1>angka2){
+    console.log(`angka ${angka1} lebih besar dari ${angka2}`)
+}else if(angka1<angka2){
+    console.log(`angka ${angka1} kurang dari ${angka2} `)
+}else{
+    console.log("nilai nya sama")
+}
---
+++ IF-ELSE/Soal3.js
@@ -0,0 +1,9 @@
+let angka = 0
+
+if(angka > 0){
+    console.log(`angka ${angka} adalah angka positif`)
+}else if(angka < 0){
+    console.log(`angka ${angka} ini adalah negatif`)
+}else{
+    console.log(`angka ${angka} ini adalah noll`)
+}
