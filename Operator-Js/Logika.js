a636b5080f33db8c28b6d2738501799cd2a426ad
FairuzzIslami <firdauz2007123@gmail.com>
2024-01-31 23:55:42 +0700

    namabah nih

---
+++ Operator-Js/Logika.js
@@ -0,0 +1,21 @@
+// operator logika adalah operator yang menggabungkan nilai variabel dan menghasilkan nilai
+// bolean /true or false
+// && => dan
+// || => atau
+// ! => kebalikan
+
+
+let nilaiUjian = 78
+let nilaiLulus = 76
+
+let hasilNilai = nilaiUjian <80
+console.log(hasilNilai)
+
+
+let hasilLulus = nilaiLulus <80
+console.log(hasilLulus)
+
+
+
+let hasil = hasilLulus && hasilNilai
+console.log(hasil)
