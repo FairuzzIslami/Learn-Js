ec91db3f8f1d72ae7c042a2226ba8d603c890f0c
FairuzzIslami <firdauz2007123@gmail.com>
2024-01-31 22:57:13 +0700

    31 januari 2024

---
+++ IF-ELSE/diskon.js
@@ -0,0 +1,7 @@
+let uang = 200
+
+if(uang >500){
+    console.log(`uang anda sekarang ${uang} rupiah jadi mendapatkan diskon sekitar 10%`)
+}else{
+    console.log(`uang anda sekarang ${uang} rupiah jadi maa   f gak ada diskon`)
+}
---
+++ IF-ELSE/segitiga.js
@@ -0,0 +1,24 @@
+// const sisi1 = 5 
+// const sisi2 = 9
+
+// if(sisi1 == sisi2){
+// console.log(`sisi 1: ${sisi1} cm dan sisi2: ${sisi2} cm,jadi ini termasuk segitiga sama sisi`)
+// } else{
+//     console.log(`sisi 1 ${sisi1} cm dan sisi2 ${sisi2} cm,jadi ini termasuk segitiga tdk beraturan`)
+// }
+
+
+let panjang = 3
+let lebar = 6
+let tinggi = 9
+
+let rumus = lebar = tinggi
+
+
+if( rumus == panjang ){
+    console.log("segitiga sama sisi")
+} else if(panjang != tinggi){
+    console.log(" segitiga sama kaki")
+}else{
+    console.log("segitiga tdk beraturan")
+}
