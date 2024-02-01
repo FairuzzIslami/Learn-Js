e92ba2a11b7cfc1668ec8c71817faf8c9cc273ba
FairuzzIslami <firdauz2007123@gmail.com>
2024-02-01 23:38:49 +0700

    1 febuari 2024

---
+++ String-Modern/main.js
@@ -0,0 +1,17 @@
+//adlah cara modern untuk menggabungkan string dan variabelmenjadi lebih mudah di gunakan
+// dgn menggunakan ` dan ${}
+
+
+
+let nama = "fairuz qila islami"
+let umur = 13
+let sekolah = "Smk muhammadiyah 04 Boyolali"
+
+
+
+// cara kuno
+console.log("hallo nama saya" +nama +"umur saya sekarang" +umur +"dan asal sekolah di" + sekolah)
+
+
+//cara modern
+console.log(`hallo nama saya ${nama} dan umur saya sekarang ${umur} dan asal sekolah dari ${sekolah}`)
