2543b566f8123f1bcd716d7ab037fdfa25f97b68
FairuzzIslami <firdauz2007123@gmail.com>
2024-02-01 23:39:46 +0700

    1 febuari 2024

---
+++ console/main.js
@@ -0,0 +1,9 @@
+// console berfungsi menampilkan codigan yg kita buat di program bahasa 
+//pemrograman javascipt dan jenis nya banyak
+// jika ingin melihatnya klik kanan kemudian klik inspek lalau cari console
+
+
+console.info("Ada informasi") //console info  ==>memberikan informasi
+console.error("tulisan eror") //console eror  ==> adanya eror
+console.log("ada boyolali cuyy") //console log ==> sama seperti info
+console.warn("cihuyyy")
