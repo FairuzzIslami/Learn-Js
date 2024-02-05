1cb2bbc0d45e0f6bf5e2f92ffce9f116aa5ae40a
FairuzzIslami <firdauz2007123@gmail.com>
2024-02-05 23:16:40 +0700

    learn-array

---
+++ Array/array.js
@@ -0,0 +1,27 @@
+//array adalah tipe data yang kumpulan data yang memiliki nilai lebih dari 1 atau lebih
+// dan di js array itu dinamis yang artinya bisa bertambah sendirinya
+// kita juga bisa menambahkan array di dalam array
+
+let nama_siswa = ["fairuz","salsa","damar","zahra","alok",2,3,4,5,6,7]
+
+
+// untukk menambahkan data di array
+nama_siswa.push("andre","hanum")
+
+
+// menapilkan data di index yang kita mau
+console.log(nama_siswa[2])
+
+
+//mengetahui panjang array
+console.log(nama_siswa.length)// 7
+
+
+// untuk mengubah data yang ada di index
+console.log(nama_siswa[2] = "ashel") // index kedua di ganti ashel
+console.log(nama_siswa[2]) 
+
+
+// menghapus data yang ada di index
+console.log(delete nama_siswa[1])  // index ke 1 jadi undifinde karena di hapus / delete
+console.log(nama_siswa[1])
