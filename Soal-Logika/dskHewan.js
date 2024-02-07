9ef1c280adabd4348a01fd4cc04966d1cdd232e4
FairuzzIslami <firdauz2007123@gmail.com>
2024-02-07 23:33:05 +0700

    new file

---
+++ Soal-Logika/dskHewan.js
@@ -0,0 +1,26 @@
+const sapi = {
+    nama : 'Sapingkk',
+    umur : null,
+    warna : "pink",
+    'jenis kelamin':"lenang",
+    suara: (nama) => {
+        console.log("cihuyy" + nama)
+    },
+    popUp: (isi) =>{
+        console.log(isi)
+        console.log(`namaku ${sapi.nama} umurku seakrrang ${sapi.umur}`) // cara 1
+    },
+    popUp2: function(){
+        console.log(`namaku ${this.nama} umurku seakrrang ${this.umur}`) // cara 1
+    }
+
+
+}
+console.log(sapi["jenis kelamin"])//
+// console.log(sapi['nama']) // cara 1
+console.log(sapi.nama)    //cara 2
+// sapi.suara('ok ok')
+// sapi.popUp('kkkk')
+// sapi.popUp('yyyyy')
+sapi.popUp()
+sapi
