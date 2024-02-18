f092823fca16439d455c30ebb362dab590d3a634
FairuzzIslami <firdauz2007123@gmail.com>
2024-02-19 00:21:17 +0700

    belajar soal array

---
+++ Latihan-SoalArray/angka.js
@@ -0,0 +1,9 @@
+let angka = [3,4,5,2,2,3,2] // 3+4+5+6+3+
+let conuter = null // null bisa, cdgak harus angka juga
+
+for(let i = 0; i < angka.length; i++){
+    conuter = conuter + angka[i] 
+    
+}
+console.log(conuter)
+
---
+++ Latihan-SoalArray/main.js
@@ -0,0 +1,14 @@
+let nama = ['fairuz','damar','zahra','salsa','ashel']
+
+for(let i = 0; i < nama.length; i++){
+    console.log(nama[i]) // buat ambil itemnya aja: sampe 5 kali ke bawah: 
+//                        fairuz
+//                         damar
+//                         zahra
+
+                        
+     console.log(nama) // dia akan  menampilkan ['fairuz','damar','zahra','salsa','ashel']
+//                      SELAMA 5 kali
+
+    console.log(i) // buat cek berapa index nya
+}
