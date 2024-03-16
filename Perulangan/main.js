19fcdc81e2fc54402c990e915f89afa434b705c5
FairuzzIslami <firdauz2007123@gmail.com>
2024-03-16 09:59:56 +0700

    for

---
+++ Perulangan/main.js
@@ -0,0 +1,16 @@
+//Perulangan (atau iterasi) adalah konsep dalam pemrograman 
+//yang memungkinkan kita untuk melakukan serangkaian instruksi atau operasi berulang kali
+
+
+
+
+
+
+// for(let i = 0; i < Array.length; i ++ ){ // ngecek array dari awal
+    // console.log(array[i])
+// }
+
+let array = [2,3,4,4,5] // elemenet terakhir
+for(let i = array.length -1; i > 0; i--){
+    console.log(array[i])
+}
