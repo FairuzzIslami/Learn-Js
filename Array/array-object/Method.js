4ec8b027eccde165c71e3d372ab161a5b9272d04
FairuzzIslami <firdauz2007123@gmail.com>
2024-04-25 22:25:17 +0700

    array-object

---
+++ Array/array-object/Method.js
@@ -0,0 +1,27 @@
+//mirip seperti array tetapi sia memiliki propetri dan value sehingga membuatnya lebih
+//detail dan kompleks dan memberikan gambaran yang deataai
+
+const kucing = {
+    nama : "Si hitam", // di m ethod gka pake = tapi make :
+    umur : 10,
+    'warna kulit' : "hitam",
+    jenis_kelamin : "lakik",
+}
+
+let ashel = {
+    nama : "adzana shalihah alifiyah",
+    umur : 18,
+    'member dari generasi': 9,
+    status : "suaminya fairuz aqila silami"
+}
+
+const data = ashel.addres.country
+console.log(data)
+
+
+console.log(kucing.nama)
+console.log(kucing['warna kulit']) // kalo variabel nya tipe string jika ingin memunculkan gunakan
+                                    //  console.log(method[variabel nya])
+
+console.log(ashel.status)
+console.log(ashel['member dari generasi'])
---
+++ Array/array-object/main.js
@@ -0,0 +1,50 @@
+const player  = [
+    {
+        name: "fairuz aqila islami",
+        age: 12,
+        kelas: "X-RPL"
+    },
+    {
+        name: "damar artha",
+        age: 13,
+        kelas: "X-TKR"
+    },
+    {
+        name: "",
+        age: 12,
+        kelas: "X-RPL"
+    },
+    {
+        name: "Adzana shalihah",
+        age: 20,
+        kelas: "X-MM"
+    }
+]
+
+// cara ingin meng output pengeluaran array object
+// nama variabel[index].propery object 
+// playe[0].name
+console.log(player[1].age)
+
+
+
+
+const Sekolah = [
+    {
+        namaSekolah : " SMK Barkab",
+        asal : "Boyolali",
+        banyakSiswa : 1232
+    },
+    {
+        namaSekolah : "SMA Teras",
+        asal : "Boyolali",
+        banyakSiswa : 1232
+    },
+    {
+        namaSekolah : "MAN BOYOLALI",
+        asal : "Boyolali",
+        banyakSiswa : 1234
+    }
+]
+
+console.log(Sekolah[2].namaSekolah)
