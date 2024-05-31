d5d5865fc82e0d2b3bbbfd2fac9141d9f4030484
Fairuz <firdauz2007123@gmail.com>
2024-05-31 22:12:31 +0700

    understanding function expretion

---
+++ Function/function-expresion.js
@@ -0,0 +1,12 @@
+// function expresion
+// memanggil function di dalam variabelll
+
+
+
+let isi = arrayForEach.forEach(function(e,i){ 
+    // parameter 1 menyebutkan isi index nya  = 'fairuz'
+    //parameter 2 menyembutkan no index nya = 0
+    console.log(`nama saya adalah ${e} dan saya index ke ${i}`)
+    return e,i
+})
+console.log(isi)
